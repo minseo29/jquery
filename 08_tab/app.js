8ee4d68306a1af531160af386e7cd7d9208a6a0a
minseo29 <heartkej@naver.com>
2022-02-25 10:16:06 +0900

    tab

---
+++ 08_tab/app.js
@@ -0,0 +1,10 @@
+// $(function () {
+//     var btns = $(".tab li")
+//     // console.log(btns)
+//     var content = $(".content")
+
+//     btns.eq(0).find("a").addClass("active")
+//     // hide-숨김
+//     content.hide().eq(0).show()
+
+// })
